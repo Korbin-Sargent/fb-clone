e864af5ecdc96509bbc9ae0a9406775b473eb69c
Korbin Sargent <korbinksargent@gmail.com>
2022-09-06 13:42:53 -0600

    Built center piece of header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
+import HeaderIcon from "./HeaderIcon";
 import {
   BellIcon,
   ChatIcon,
@@ -15,29 +16,42 @@ import {
   ShoppingCartIcon,
 } from "@heroicons/react/24/outline";
 
-
 function Header() {
   return (
-    <div>
-        {/* Header Left */}
-        <div className="flex items-center">
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      {/* Header Left */}
+      <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
           height={40}
           layout="fixed"
+        />
+        <div className="flex items-center ml-2 rounded-full bg-gray-100 p-2">
+          <MagnifyingGlassIcon className="h-6 text-gray-600" />
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Feed"
           />
-          </div>
-    <div className="flex">
-      <MagnifyingGlassIcon className="h-6"/>
-      <input type="text" placeholder='Search Feed' />
-    </div>
+        </div>
+      </div>
 
-        {/* Header Center */}
-        {/* Header Right */}
+      {/* Header Center */}
+      {/* flex-grow to expand the center div as much as posisible */}
+      <div className="flex justify-center flex-grow ">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
 
+      {/* Header Right */}
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
