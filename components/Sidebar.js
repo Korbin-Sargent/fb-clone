85be663cddc304dafbc9c2201d29de03b7edbbd7
Korbin Sargent <korbinksargent@gmail.com>
2022-09-09 12:27:07 -0600

    Built reusable components for side bar rows

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,12 +11,14 @@ import {
   ComputerDesktopIcon,
   UserGroupIcon,
 } from "@heroicons/react/24/outline";
+import SidebarRow from "./SidebarRow";
 
 function Sidebar() {
   const { data: session } = useSession();
 
   return (
     <div className="p-2 mt-5 max-w-[600px] xl:min-w-[300px]">
+      <SidebarRow src={session.user.image} title={session.user.name} />
       <SidebarRow Icon={UsersIcon} title="Friends" />
       <SidebarRow Icon={UserGroupIcon} title="Groups" />
       <SidebarRow Icon={ShoppingBagIcon} title="Marketplace" />
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Header from "../components/Header";
 import Login from "../components/Login";
+import Sidebar from "../components/Sidebar";
 
 export default function Home({ session }) {
   if (!session) return <Login />;
