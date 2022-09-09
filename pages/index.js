484c7a69f409f5502b5804d0a2cf9eccce6f7744
Korbin Sargent <korbinksargent@gmail.com>
2022-09-09 14:06:50 -0600

    building out story cards

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Header from "../components/Header";
 import Login from "../components/Login";
 import Sidebar from "../components/Sidebar";
+import Feed from "../components/Feed";
 
 export default function Home({ session }) {
   if (!session) return <Login />;
@@ -15,10 +16,11 @@ export default function Home({ session }) {
       {/* header */}
       <Header />
 
-      <main>
+      <main className="flex">
         {/* sidebar */}
         <Sidebar />
         {/* feed */}
+        <Feed />
         {/* widgets */}
       </main>
     </div>
